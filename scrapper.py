8537df032b800176a1643be46ce1d369c5fe4c3c
Karl Steven <54846911+karlorjalo@users.noreply.github.com>
2020-11-25 12:42:10 +0800

    Add files via upload

---
+++ scrapper.py
@@ -0,0 +1,19 @@
+from urllib.request import Request, urlopen
+
+s = ","
+i = 8
+count = 0
+url = Request('https://www.fairprice.com.sg/product/13111689', headers={'User-Agent': 'Mozilla/5.0'})
+page = urlopen(url)
+
+html_bytes = page.read()
+html = html_bytes.decode("utf-8")
+
+priceFinder = html[html.find('\\"price\\":')+13:html.find('\\"price\\":')+20]
+priceFix = [x for x in priceFinder if x not in ["'",'"',"\\",","]]
+price = float("".join(priceFix))
+print(round(price, 2))
+
+
+    
+
