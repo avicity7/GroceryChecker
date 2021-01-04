3a4853415ae8e2511cbca3f98af3a63c4199286e
Karl Steven <54846911+karlorjalo@users.noreply.github.com>
2021-01-04 23:05:14 +0800

    Update scrapper.py
    Pushed Finder V0.1

--- scrapper.py
+++ scrapper.py
@@ -1,7 +1,42 @@
 from urllib.request import Request, urlopen
+from requests_html import HTMLSession
 
+session = HTMLSession()
+while True:
+    inp = input("Please input the item that you would like to find. Please be as specific as possible:")
+    sep = "%20"
+    inp = inp.split()
+    inp = sep.join(inp)
+    r = session.get('https://www.fairprice.com.sg/search?query=' + inp)
+    links = r.html.links
+    e = [x for x in links]
+    for y in e:
+        try:
+            if y[0]+y[1]+y[2]+y[3]+y[4]+y[5]+y[6]+y[7] == "/product":
+                product = y
+        except:
+            break
+    url = Request('https://www.fairprice.com.sg' + product, headers={'User-Agent': 'Mozilla/5.0'})
+    page = urlopen(url)
+
+    html_bytes = page.read()
+    html = html_bytes.decode("utf-8")
+
+    nameFinder = html[html.find('\\"name\\":')+2:html.find('\\"name\\":')+30]
+    nameFix = [x for x in nameFinder if x not in ["'",'"',"\\",",","}",";",")"]]
+    productName = str("".join(nameFix))
+    print("Is this what you were looking for? ||"+productName+"||")
+    inp = input("Type yes or no:")
+    if inp == "yes":
+        print("Great!")
+        break
+    else:
+        continue
+
+
+"""
 # FairPrice
-url = Request('https://www.fairprice.com.sg/product/13111689', headers={'User-Agent': 'Mozilla/5.0'})
+url = Request(link, headers={'User-Agent': 'Mozilla/5.0'})
 page = urlopen(url)
 
 html_bytes = page.read()
@@ -11,7 +46,9 @@ priceFinder = html[html.find('\\"price\\":')+13:html.find('\\"price\\":')+20]
 priceFix = [x for x in priceFinder if x not in ["'",'"',"\\",",","}",";",")"]]
 price = float("".join(priceFix))
 print(round(price, 2))
+"""
 
+"""
 # Redmart
 url = Request('https://www.lazada.sg/products/gala-apples-i301076962-s527122210.html?spm=a2o42.lazmart_search.list.1.75343f2aSWJ1G1&search=1', headers={'User-Agent': 'Mozilla/5.0'})
 page = urlopen(url)
@@ -107,3 +144,4 @@ priceFinder = html[html.find('price:amount\" content=\"')+23:html.find('price:am
 priceFix = [x for x in priceFinder if x not in ["'",'"',"\\",",","}",";",")"]]
 price = float("".join(priceFix))
 print(round(price, 2))
+"""
