b1445b22a18f4f155a81bc7f92c2327373204eb2
Karl Steven <54846911+karlorjalo@users.noreply.github.com>
2021-01-24 17:50:01 +0800

    Added exception list for unitFix

--- main/ScrapperBS.py
+++ main/ScrapperBS.py
@@ -3,7 +3,7 @@ from urllib.request import Request, urlopen
 unit = str()
 temp = int()
 #url = Request('https://www.fairprice.com.sg' + product, headers={'User-Agent': 'Mozilla/5.0'})
-url = Request('https://www.fairprice.com.sg/product/canard-duchene-charles-vii-blanc-de-blancs-6-x-750ml-900501913', headers={'User-Agent': 'Mozilla/5.0'})
+url = Request('https://www.fairprice.com.sg/product/canard-duchene-charles-vii-blanc-de-blancs-6-x-750ml-90050191', headers={'User-Agent': 'Mozilla/5.0'})
 page = urlopen(url)
 html = page.read().decode("utf-8")
 soup = BeautifulSoup(html, "html.parser")
@@ -19,7 +19,7 @@ name = text[0:text.find("|")-1]
 unitFinder = soup.findAll("div", {"class": "sc-13n2dsm-10 cpkeZQ"})
 unitFix = str(unitFinder)[133:]
 for i in unitFix:
-    if i != "<":
+    if i not in ["<", '"', "'", "!", "/"]:
         unit += i 
     else: 
         break 
